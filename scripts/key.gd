4d5a93ba8580145a5404f9132e74ab56d3c8c3c2
Alexandre Nogueira <shsljuu2@gmail.com>
2025-09-09 21:15:30 -0300

    make it so the key sprite changes with its movement direction

--- scripts/key.gd
+++ scripts/key.gd
@@ -1,9 +1,12 @@
 extends PathFollow2D
-@export var SPEED = 150.0
-var direction = -1
-@onready var rayCastUp = $RayCastUp
-@onready var rayCastDown = $RayCastDown
+@export var SPEED = 100.0
+
 @onready var animatedSprite = $AnimatedSprite2D
+var previous_pos: Vector2
+
+func _ready() -> void:
+	previous_pos = global_position
+	
 func _process(delta):
 	'''
 	if rayCastDown.is_colliding():
@@ -17,6 +20,22 @@ func _process(delta):
 
 func _physics_process(delta: float) -> void:
 	progress += SPEED * delta
-	if progress_ratio >= 1.0:
+	var dir := (global_position - previous_pos).normalized()
+	print(dir)
+	previous_pos = global_position
+	if abs(dir.x) > abs(dir.y):
+		if dir.x > 0:
+			_set_animation("rightanim")
+		else:
+			_set_animation("leftanim")
+	else:
+		if dir.y > 0:
+			_set_animation("downanim")
+		else:
+			_set_animation("upanim")
+	if progress_ratio >= 0.97:
 		gameManager.take_damage(1)
 		queue_free() 
+func _set_animation(name: String):
+	if animatedSprite.animation != name:
+		animatedSprite.play(name)
