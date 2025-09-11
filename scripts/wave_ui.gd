38d4f136dc7fa6b4bc3e5c47d97a4a08b4bb4ac9
Alexandre Nogueira <shsljuu2@gmail.com>
2025-09-11 02:46:54 -0300

    add wave warning animation

--- scripts/wave_ui.gd
+++ scripts/wave_ui.gd
@@ -1,8 +1,14 @@
 extends Control
 @onready var wave_counter = $wave_counter
+@onready var warning = $warning
+@onready var warning_shadow = $warning/warningShadow
+@onready var anim_text = $AnimationPlayer
 func _ready() -> void:
 	gameManager.connect("wave_changed", Callable(self, "_on_wave_change"))
 	wave_counter.text = str(gameManager.current_wave+1) + "/" + str(gameManager.max_wave)
 func _on_wave_change(new_wave):
 	wave_counter.text = str(new_wave+1) + "/" + str(gameManager.max_wave)
+	warning.text = "WAVE" + str(new_wave+1) + "!"
+	warning_shadow.text = "WAVE" + str(new_wave+1) + "!"
+	anim_text.play("text anim")
 	
